e182cb97575118cf6b2644f802c6f0ef89cbcd45
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-02-03 14:23:19 +0300

    Don't remove zero-width characters as they have certain functions
    E.g. emojis can be modified with a zero-width character to join two
    emojis together to create a new one.

--- src/cleaners/cleanDisplayName.js
+++ src/cleaners/cleanDisplayName.js
@@ -8,7 +8,6 @@ const cleanDisplayName = (displayName) => {
   /* eslint-disable no-control-regex */
   cleaned = cleaned.replace(/[\u0000-\u001F\u007F\u0080-\u009F]/g, ''); // ASCII control characters.
   cleaned = cleaned.replace(/[\u000A-\u000D\u0085\u2028\u2029]/g, ''); // Line breaks.
-  cleaned = cleaned.replace(/[\u200B-\u200D\uFEFF\u2063\u2800\u180E]/g, ''); // Zero-width spaces.
   cleaned = cleaned.trim();
 
   return cleaned;
