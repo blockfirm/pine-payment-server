0b619273a10a627994687f777f4d82503ce24357
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-06-10 19:40:36 +0200

    Add support for db port and dialect options

--- src/database/DatabaseClient.js
+++ src/database/DatabaseClient.js
@@ -17,7 +17,9 @@ export default class DatabaseClient {
 
     this.sequelize = new Sequelize(config.database, config.username, config.password, {
       host: config.host,
+      port: config.port,
       dialect: config.dialect,
+      dialectOptions: config.dialectOptions,
       logging: false
     });
 
