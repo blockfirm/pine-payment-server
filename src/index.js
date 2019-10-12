6f019d41dc6537465c39edabb7f0eb63a5824915
Timothy Engqvist Johansson <timothy@blockfirm.se>
2019-10-12 18:54:11 +0200

    Gracefully shut down server on SIGINT

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ import config from './config';
 const server = new Server(config);
 
 server.start();
+
+const onExit = () => {
+  console.log('Gracefully shutting down...');
+
+  server.stop()
+    .then(() => {
+      console.log('Graceful shutdown completed');
+      return 0;
+    })
+    .catch(error => {
+      console.log('Graceful shutdown failed with error:', error.message);
+      return 1;
+    })
+    .then(code => {
+      process.kill(code);
+    });
+};
+
+process.stdin.resume();
+process.on('SIGINT', onExit);
